8c20a3b84695b77ffce138498923ffee1c0c92f9
oleg_melnyk <olegmelnyk237@gmail.com>
2025-04-14 23:12:49 +0300

    Add files via upload

---
+++ ListVSHashSet/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+
+class Program
+{
+    static void Main()
+    {
+        const int n = 100_000;
+        var list = new List<int>();
+        var set = new HashSet<int>();
+
+        for (int i = 0; i < n; i++)
+        {
+            list.Add(i);
+            set.Add(i);
+        }
+
+        // REMOVE from List
+        var swList = Stopwatch.StartNew();
+        for (int i = 0; i < n; i++)
+        {
+            list.Remove(i);
+        }
+        swList.Stop();
+        Console.WriteLine($"List.Remove(item): {swList.ElapsedMilliseconds} ms");
+
+        // REMOVE from HashSet
+        var swSet = Stopwatch.StartNew();
+        for (int i = 0; i < n; i++)
+        {
+            set.Remove(i);
+        }
+        swSet.Stop();
+        Console.WriteLine($"HashSet.Remove(item): {swSet.ElapsedMilliseconds} ms");
+    }
+}
